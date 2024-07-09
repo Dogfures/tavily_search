63d12bfa5ebbb6cb1f34995bd4e1c6e9776fceee
Alexis PREVOT <a.prevot@thecodingmachine.com>
2024-07-09 22:08:25 +0200

    adding fix

--- app_tavily_search_agent.py
+++ app_tavily_search_agent.py
@@ -3,26 +3,26 @@ import os
 import streamlit as st
 
 from langchain_openai import ChatOpenAI
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from langchain.agents import AgentExecutor, create_react_agent, create_tool_calling_agent, create_openai_functions_agent
-from langgraph.checkpoint.sqlite import SqliteSaver
-from langchain_community.agent_toolkits import create_sql_agent
+#from langchain_community.agent_toolkits import SQLDatabaseToolkit
+#from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain.agents import AgentExecutor, create_openai_functions_agent
+#from langgraph.checkpoint.sqlite import SqliteSaver
+#from langchain_community.agent_toolkits import create_sql_agent
 from langchain import hub
 from langchain_community.callbacks.streamlit import (
     StreamlitCallbackHandler,
 )
-from langchain_community.agent_toolkits.sql.prompt import SQL_FUNCTIONS_SUFFIX
-from langchain_core.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    MessagesPlaceholder,
-)
+#from langchain_community.agent_toolkits.sql.prompt import SQL_FUNCTIONS_SUFFIX
+#from langchain_core.prompts.chat import (
+#    ChatPromptTemplate,
+#    HumanMessagePromptTemplate,
+#    MessagesPlaceholder,
+#)
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 import time
-import llm_logic
-import init_db
+#import llm_logic
+#import init_db
 #from langchain.globals import set_verbose
 
 #set_verbose(True)
